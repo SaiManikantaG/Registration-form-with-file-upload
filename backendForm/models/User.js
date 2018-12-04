80b2f44a1a1e3ffa7b131d3a25e485d488a1d95e
Sai Manikanta Godavarthi <saig2304@gmail.com>
2018-12-04 10:32:46 -0600

    Update User.js

--- backendForm/models/User.js
+++ backendForm/models/User.js
@@ -11,17 +11,3 @@ module.exports = mongoose.model('User', {
   sign: String,
   FileName: String
 })
-
-
-//create user model for all the fields given in the front end. I am not sure about attachments
-/* 
-create model for this object here:
-name: "Nimisha Bhushan Niyogi" => String, 
-wsu: " das" => String, 
-email: "nbniyogi@shockers.wichita.edu" => String, 
-phone: 3165167431 => String ,
-Color:"Grey" => String,
-Subscribe:true => Boolean
-details:"dagsg" => String
-sign:" agdgag"  => String
-*/
